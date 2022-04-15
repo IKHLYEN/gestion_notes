7646bc01f6619c9c4a11a9689d21127ac306660d
pc <pc@192.168.65.1>
2022-04-15 17:02:43 +0200

    equals

--- gestion_notes/src/gestion_notes/Etudiant.java
+++ gestion_notes/src/gestion_notes/Etudiant.java
@@ -3,10 +3,15 @@ package gestion_notes;
 public class Etudiant {
 	private int id;
 	private String nom;
-	private double note=10;
+	private double note;
 	public Etudiant(int i, String n) {
+		this.note=10;
 		nom=n;
 		id=i;
 		
 	}
+	@Override
+	public String toString() {
+		return this.nom+ ":"+this.note;
+	} 
 }
