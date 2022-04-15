25e923d08bc12c87805f1814d914ce5330f851b3
pc <pc@192.168.65.1>
2022-04-15 18:25:22 +0200

    mesEtudiants

--- gestion_notes/src/gestion_notes/Main.java
+++ gestion_notes/src/gestion_notes/Main.java
@@ -6,7 +6,13 @@ public class Main {
 		Etudiant e1 = new Etudiant(1,"hamid");
 		Etudiant e2 = new Etudiant(2,"driss");
 		Etudiant e3= new Etudiant(3,"youssef");
-		
+		e1.note=15;
+		e2.note=12;
+		e3.note=16;
+		ListEtudiants mesEtudiants=new ListEtudiants();
+		mesEtudiants.add(e3);
+		mesEtudiants.add(e2);
+		mesEtudiants.add(e1);
 	}
 
 }
