dcee2a2400e93c60b05a6ae1ab3638bd0d5c1659
pc <pc@192.168.65.1>
2022-04-15 18:16:32 +0200

    creation du 3 etudiants

--- gestion_notes/src/gestion_notes/Condition.java
+++ gestion_notes/src/gestion_notes/Condition.java
@@ -1,5 +1,9 @@
 package gestion_notes;
 
 public interface Condition {
-
+public boolean estVrai(Etudiant e);
+public List<Etudiant> filtrer(Condition c)
+{
+	return Etudiant.c;
+}
 }
---
+++ gestion_notes/src/gestion_notes/Main.java
@@ -0,0 +1,12 @@
+package gestion_notes;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Etudiant e1 = new Etudiant(1,"hamid");
+		Etudiant e2 = new Etudiant(2,"driss");
+		Etudiant e3= new Etudiant(3,"youssef");
+		
+	}
+
+}
